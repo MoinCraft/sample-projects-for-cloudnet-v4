1395e01e380dd7c10439533e029ae915827c01b3
Marvin <GiantTreeLP@users.noreply.github.com>
2024-04-13 19:48:24 +0200

    fix(maven+gradle+bukkit+plugin): Update to the latest Bukkit/Spigot and CloudNet versions for this project

--- gradle/bukkit/plugin/build.gradle.kts
+++ gradle/bukkit/plugin/build.gradle.kts
@@ -6,6 +6,7 @@ group = "com.github.moincraft.gradle.bukkit"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven {
         name = "spigot-repo"
@@ -15,13 +16,14 @@ repositories {
 
 dependencies {
     // Spigot
-    val spigotVersion = "1.20.2-R0.1-SNAPSHOT"
+    val spigotVersion = "1.20.4-R0.1-SNAPSHOT"
     implementation("org.spigotmc", "spigot-api", spigotVersion)
 
     // CloudNet
-    val cloudNetVersion = "4.0.0-RC9"
-    implementation("eu.cloudnetservice.cloudnet", "bridge", cloudNetVersion)
-    implementation("eu.cloudnetservice.cloudnet", "platform-inject-api", cloudNetVersion)
+    val cloudNetVersion = "4.0.0-RC10"
+    implementation(platform("eu.cloudnetservice.cloudnet:bom:$cloudNetVersion"))
+    implementation("eu.cloudnetservice.cloudnet", "bridge")
+    implementation("eu.cloudnetservice.cloudnet", "platform-inject-api")
     annotationProcessor("eu.cloudnetservice.cloudnet", "platform-inject-processor", cloudNetVersion)
 }
 

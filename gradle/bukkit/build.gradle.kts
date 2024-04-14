c11790002e7f4c53d1f5808f70ec234c9b5f43c3
Marvin <GiantTreeLP@users.noreply.github.com>
2024-04-14 12:34:35 +0200

    chore(project): Simplify Gradle build files

--- gradle/bukkit/build.gradle.kts
+++ gradle/bukkit/build.gradle.kts
@@ -1,18 +1,2 @@
-plugins {
-    id("java")
-}
-
 group = "com.github.moincraft.gradle"
 version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
